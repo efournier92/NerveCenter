e9339b8321613d8390e04166ec4de4e36f25d5ea
Misigno <efournier92@gmail.com>
2016-12-12 20:11:52 -0500

    write addWidgets function

--- public/dashboard.js
+++ public/dashboard.js
@@ -13,6 +13,18 @@ tabClear.controller('dashboardController', ['$scope', function($scope) {
     { sizeX: 1, sizeY: 1, row: 2, col: 3 },
     { sizeX: 1, sizeY: 1, row: 2, col: 4 },
   ];
-}]);
 
+
+  const addWidgets = function addWidgets() {
+    $scope.widgets = [];
+    for (i = 0; i < 1; i++) {
+      for (j = 0; j < 5; j++) {
+        newWidget = { sizeX: 1, sizeY: 1, row: i, col: j };
+        $scope.widgets.push(newWidget);
+      }
+    }
+  };
+  addWidgets();
+  console.log($scope.widgets);
+}]);
 
