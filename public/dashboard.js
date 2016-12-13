24b3e13475a895c6125b9becab821895047e3825
Misigno <efournier92@gmail.com>
2016-12-12 20:11:16 -0500

    change to 5 columns

--- public/dashboard.js
+++ public/dashboard.js
@@ -7,15 +7,11 @@ tabClear.controller('dashboardController', ['$scope', function($scope) {
     { sizeX: 1, sizeY: 1, row: 0, col: 2 },
     { sizeX: 1, sizeY: 1, row: 0, col: 3 },
     { sizeX: 1, sizeY: 1, row: 0, col: 4 },
-    { sizeX: 1, sizeY: 1, row: 0, col: 5 },
-    { sizeX: 1, sizeY: 1, row: 0, col: 6 },
     { sizeX: 1, sizeY: 1, row: 2, col: 0 },
     { sizeX: 1, sizeY: 1, row: 2, col: 1 },
     { sizeX: 1, sizeY: 1, row: 2, col: 2 },
     { sizeX: 1, sizeY: 1, row: 2, col: 3 },
     { sizeX: 1, sizeY: 1, row: 2, col: 4 },
-    { sizeX: 1, sizeY: 1, row: 2, col: 5 },
-    { sizeX: 1, sizeY: 1, row: 2, col: 6 },
   ];
 }]);
 
