f38275074e6acc47e1fb7a012664b96445d13d09
Misigno <efournier92@gmail.com>
2016-12-15 18:44:52 -0500

    set password with crypto

--- server.js
+++ server.js
@@ -101,4 +101,12 @@ var userSchema = new mongoose.Schema({
   hash: String,
   salt: String
 });
+
+var crypto = require('crypto');
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
 });
