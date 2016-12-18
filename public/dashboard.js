d39570e29036eb12878697de9cdf5e6cdb47d2af
Misigno <efournier92@gmail.com>
2016-12-18 11:34:51 -0500

    create defaults.json file

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,178 +1,60 @@
 var tabClear = angular.module('tabClear', ['ngRoute', 'gridster', 'pr.longpress']);
 
 tabClear.controller('dashboardController', ['$scope', function($scope) {
-  $scope.onLongPress = function () {
-    console.log("DO IT!!");
-  };
+    $scope.onLongPress = function() {
+        console.log("DO IT!!");
+    };
 
-  // module.exports.register = function(req, res) {
-  //   console.log("Registering user: " + req.body.email);
-  //   res.status(200);
-  //   res.json({
-  //     "message" : "User registered: " + req.body.email
-  //   });
-  // };
-  $scope.widgets = [
-    { sizeX: 1,
-      sizeY: 1,
-      row: 0,
-      col: 0,
-      url: "https://www.google.com/",
-      icon: "icons/Google.png" },
-      { sizeX: 1,
-        sizeY: 1,
-        row: 0,
-        col: 1,
-        url: "http://en.wikipedia.org/wiki/Main_Page",
-        icon: "icons/Wiki.png" },
-        { sizeX: 1,
-          sizeY: 1,
-          row: 0,
-          col: 2,
-          url: "http://cake.whatbox.ca:57094/",
-          icon: "icons/RTorrent.png" },
-          { sizeX: 1,
-            sizeY: 1,
-            row: 0,
-            col: 3,
-            url: "https://github.com/",
-            icon: "icons/GitHub.png" },
-            { sizeX: 1,
-              sizeY: 1,
-              row: 0,
-              col: 4,
-              url: "https://twitter.com",
-              icon: "icons/Twitter.png" },
-              { sizeX: 1,
-                sizeY: 1,
-                row: 1,
-                col: 0,
-                url: "https://www.google.com/imghp?hl=en&tab=wi&ei=KA6OU4CWBtDisATKzoKwBA&ved=0CAQQqi4oAg",
-                icon: "icons/Image.png" },
-                { sizeX: 1,
-                  sizeY: 1,
-                  row: 1,
-                  col: 1,
-                  url: "https://getpocket.com/a/queue/list/",
-                  icon: "icons/ReadLater.png" },
-                  { sizeX: 1,
-                    sizeY: 1,
-                    row: 1,
-                    col: 2,
-                    url: "http://www.youtube.com/",
-                    icon: "icons/Tube.png" },
-                    { sizeX: 1,
-                      sizeY: 1,
-                      row: 1,
-                      col: 3,
-                      url: "https://app.simplenote.com/",
-                      icon: "icons/Notes.png" },
-                      { sizeX: 1,
-                        sizeY: 1,
-                        row: 1,
-                        col: 4,
-                        url: "https://www.linkedin.com",
-                        icon: "icons/Linked.png" },
-                        { sizeX: 1,
-                          sizeY: 1,
-                          row: 1,
-                          col: 5,
-                          url: "http://www.cnn.com/",
-                          icon: "icons/CNN.png" },
-                          { sizeX: 1,
-                            sizeY: 1,
-                            row: 1,
-                            col: 4,
-                            url: "",
-                            icon: "" },
-                            { sizeX: 1,
-                              sizeY: 1,
-                              row: 2,
-                              col: 0,
-                              url: "",
-                              icon: "" },
-                              { sizeX: 1,
-                                sizeY: 1,
-                                row: 2,
-                                col: 1,
-                                url: "",
-                                icon: "" },
-                                { sizeX: 1,
-                                  sizeY: 1,
-                                  row: 2,
-                                  col: 2,
-                                  url: "",
-                                  icon: "" },
-                                  { sizeX: 1,
-                                    sizeY: 1,
-                                    row: 2,
-                                    col: 3,
-                                    url: "",
-                                    icon: "" },
-                                    { sizeX: 1,
-                                      sizeY: 1,
-                                      row: 2,
-                                      col: 4,
-                                      url: "",
-                                      icon: "" },
-                                      { sizeX: 1,
-                                        sizeY: 1,
-                                        row: 2,
-                                        col: 5,
-                                        url: "",
-                                        icon: "" },
-                                        { sizeX: 1,
-                                          sizeY: 1,
-                                          row: 3,
-                                          col: 0,
-                                          url: "",
-                                          icon: "" }
+    // module.exports.register = function(req, res) {
+    //   console.log("Registering user: " + req.body.email);
+    //   res.status(200);
+    //   res.json({
+    //     "message" : "User registered: " + req.body.email
+    //   });
+    // };
 
-  ];
+    // for (i = 0; i < 4; i++) {
+    //   for (j = 0; j < 5; j++) {
+    //     $scope.widgets.push(newWidget);
+    //   }
+    // }
 
-  // for (i = 0; i < 4; i++) {
-  //   for (j = 0; j < 5; j++) {
-  //     $scope.widgets.push(newWidget);
-  //   }
-  // }
-
-  $scope.gridOptions = {
-    columns: 5,
-    pushing: true,
-    floating: true,
-    swapping: true,
-    width: 'auto',
-    colWidth: 'auto',
-    rowHeight: 'match',
-    margins: [10, 10],
-    outerMargin: true,
-    sparse: false,
-    isMobile: false,
-    mobileBreakPoint: 600,
-    mobileModeEnabled: true,
-    minColumns: 1,
-    minRows: 1,
-    maxRows: 100,
-    defaultSizeX: 1,
-    defaultSizeY: 1,
-    minSizeX: 1,
-    maxSizeX: null,
-    minSizeY: 1,
-    maxSizeY: null,
-    resizable: {
-      enabled: false,
-      handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
-      start: function(event, $element, widget) {},
-      resize: function(event, $element, widget) {},
-      stop: function(event, $element, widget) {}
-    },
-    draggable: {
-      enabled: false,
-      handle: '.my-class',
-      start: function(event, $element, widget) {},
-      drag: function(event, $element, widget) {},
-      stop: function(event, $element, widget) {}
-    }
-  };
+    $scope.gridOptions = {
+      columns: 5,
+      pushing: true,
+      floating: true,
+      swapping: true,
+      width: 'auto',
+      colWidth: 'auto',
+      rowHeight: 'match',
+      margins: [10, 10],
+      outerMargin: true,
+      sparse: false,
+      isMobile: false,
+      mobileBreakPoint: 600,
+      mobileModeEnabled: true,
+      minColumns: 1,
+      minRows: 1,
+      maxRows: 100,
+      defaultSizeX: 1,
+      defaultSizeY: 1,
+      minSizeX: 1,
+      maxSizeX: null,
+      minSizeY: 1,
+      maxSizeY: null,
+      resizable: {
+        enabled: false,
+        handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
+        start: function(event, $element, widget) {},
+        resize: function(event, $element, widget) {},
+        stop: function(event, $element, widget) {}
+      },
+      draggable: {
+        enabled: false,
+        handle: '.my-class',
+        start: function(event, $element, widget) {},
+        drag: function(event, $element, widget) {},
+        stop: function(event, $element, widget) {}
+      }
+    };
 }]);
-
