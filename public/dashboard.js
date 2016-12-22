bd4e134bd1f71af15f6ff00301ce5bd9080f5c9a
Misigno <efournier92@gmail.com>
2016-12-21 23:10:08 -0500

    change name to nerveCenter in Angular

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,6 +1,6 @@
-var tabClear = angular.module('tabClear', ['ngRoute', 'gridster', 'pr.longpress']);
+var nerveCenter = angular.module('nerveCenter', ['ngRoute', 'gridster', 'pr.longpress']);
 
-tabClear.controller('dashboardController', ['$scope', '$http', function($scope, $http) {
+nerverCenter.controller('dashboardController', ['$scope', '$http', function($scope, $http) {
   $scope.onLongPress = function() {
     console.log("DO IT!!");
   };
