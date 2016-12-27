679de01cae7135d21e848c1e247872e2714a3884
Misigno <efournier92@gmail.com>
2016-12-27 13:29:16 -0500

    comment out morgan in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var app      = express();
 // var mongoose = require('mongoose');
 
-var morgan = require('morgan');
+// var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var passport = require('passport');
