bb6df0f241352d3cfd18715893551b4ada1ce739
Misigno <efournier92@gmail.com>
2017-01-01 13:50:59 -0500

    add user generateJwt method

--- app/user/user.model.js
+++ app/user/user.model.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var user = new mongoose.Schema({
   email: {
@@ -22,4 +23,16 @@ user.methods.setPassword = function(password){
 userSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
   return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
 };
