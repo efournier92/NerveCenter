027ea76dfda41eabcf7569421972a56f6bb9cbc0
Misigno <efournier92@gmail.com>
2017-03-02 19:20:16 -0500

    refactor profile.js

--- app_api/controllers/profile.js
+++ app_api/controllers/profile.js
@@ -10,31 +10,16 @@ module.exports.profileRead = function(req, res) {
     User.findById(req.payload._id)
       .exec(function(err, user) {
         res.status(200).json(user);
-        console.log("users!", user.email);
       });
   }
 };
 
 module.exports.updateWidgets = function(req, res) {
-  console.log("SERVER: ", req);
-
   if (!req.payload._id) {
     res.status(401).json({
-      "message" : "EAZY!UnauthorizedError: private profile"
+      "message" : "UnauthorizedError: private profile"
     });
   } else {
-    // User.findById(req.payload._id)
-    // .exec(function(err, user) {
-    //   if (err)
-    //     res.send(err);
-      // pry = require('pryjs')
-      // user.widgets = req.body;
-
-      // User.update(
-      //   { id: req.payload._id },
-      //   { widgets: 'STRING' }
-      // )
-
     User.findById(req.payload._id, function (err, user) {
       if (err) return handleError(err);
 
@@ -44,11 +29,5 @@ module.exports.updateWidgets = function(req, res) {
         res.send(updatedUser);
       });
     });
-    console.log("DID IT@@@!!!!")
-    // user.save(function(err) {
-    //   if (err)
-    //     res.send(err);
-    //   console.log("User Updated: ", user);
-    // });
   };
 };
