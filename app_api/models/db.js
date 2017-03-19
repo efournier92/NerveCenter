04f9f3bcbe082e5c17012ec11419a07a82fb61a8
Misigno <efournier92@gmail.com>
2017-03-19 16:02:57 -0400

    refactor db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,10 +1,15 @@
+//////////////////////////////////////////////////////////////////////
+// DEPENDENCIES
+//////////////////////////////////////////////////////////////////////
 var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/NerveCenter';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
-
+//////////////////////////////////////////////////////////////////////
+// MONGOOSE 
+//////////////////////////////////////////////////////////////////////
 mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
@@ -17,33 +22,32 @@ mongoose.connection.on('error', function(err) {
 mongoose.connection.on('disconnected', function() {
   console.log('Mongoose disconnected');
 });
-
-// CAPTURE APP TERMINATION / RESTART EVENTS
-// To be called when process is restarted or terminated
+//////////////////////////////////////////////////////////////////////
+// APP TERMINATION EVENTS
+//////////////////////////////////////////////////////////////////////
 gracefulShutdown = function(msg, callback) {
   mongoose.connection.close(function() {
     console.log('Mongoose disconnected through ' + msg);
     callback();
   });
 };
-// For nodemon restarts
 process.once('SIGUSR2', function() {
   gracefulShutdown('nodemon restart', function() {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
-// For app termination
 process.on('SIGINT', function() {
   gracefulShutdown('app termination', function() {
     process.exit(0);
   });
 });
-// For Heroku app termination
 process.on('SIGTERM', function() {
   gracefulShutdown('Heroku app termination', function() {
     process.exit(0);
   });
 });
-
-// BRING IN YOUR SCHEMAS & MODELS
+//////////////////////////////////////////////////////////////////////
+// MODELS
+//////////////////////////////////////////////////////////////////////
 require('./users');
+
