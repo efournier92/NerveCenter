07b182e5838fb6a70184fbf4f80b745cb6833228
Misigno <efournier92@gmail.com>
2017-04-23 13:11:01 -0400

    delete name from user object (client-side)

--- app_api/controllers/auth.controller.js
+++ app_api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.register = function(req, res) {
 
-  if(!req.body.name || !req.body.email || !req.body.password) {
+  if(!req.body.email || !req.body.password) {
     sendJSONresponse(res, 400, {
       "message": "All fields required"
     });
@@ -18,7 +18,6 @@ module.exports.register = function(req, res) {
 
   var user = new User();
 
-  user.name = req.body.name;
   user.email = req.body.email;
 
   user.setPassword(req.body.password);
