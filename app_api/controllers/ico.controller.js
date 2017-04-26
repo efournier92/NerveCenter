95218ff2faa4144bffeffa6978fdebdc70bbc8c0
Misigno <efournier92@gmail.com>
2017-04-26 18:30:13 -0400

    refactor app.js

--- app_api/controllers/ico.controller.js
+++ app_api/controllers/ico.controller.js
@@ -6,14 +6,14 @@ module.exports.getIcons = function(req, res) {
 
   fs.readdir(path, function(err, files) {
     if (err) {
-    res.status(400).json({
-      'message' : 'Directory Not Found'
-    });
+      res.status(400).json({
+        'message' : 'Directory Not Found'
+      });
     }
+
     files.forEach(function(ico) {
       icoList.push(ico) 
     });
-
     res.status(200).json(icoList);
   });
 };
