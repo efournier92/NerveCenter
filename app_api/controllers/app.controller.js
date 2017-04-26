0226e27e791e5e84044be01549b23cd998cc5f34
Misigno <efournier92@gmail.com>
2017-04-25 22:14:41 -0400

    finish api app.controller

--- app_api/controllers/app.controller.js
+++ app_api/controllers/app.controller.js
@@ -2,12 +2,18 @@ var fs = require('fs');
 
 module.exports.getIcons = function(req, res) {
   var path = './public/images/icons';
-  var iconList = [];
+  var icoList = [];
 
   fs.readdir(path, function(err, files) {
-    if (err) return;
-    files.forEach(function(f) {
-      console.log('Files: ' + f);
+    if (err) {
+      sendJSONresponse(res, 400, {
+        "message": "Directory not found"
+      });
+    }
+    files.forEach(function(ico) {
+      icoList.push(ico) 
     });
+
+    res.status(200).json(icoList);
   });
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,18 +7,22 @@ var auth = jwt({
   userProperty: 'payload'
 });
 
-var userCtrl = require('../controllers/user.controller');
 var authCtrl = require('../controllers/auth.controller');
+var appCtrl = require('../controllers/app.controller');
+var userCtrl = require('../controllers/user.controller');
+
+// Auth API 
+router.post('/register', authCtrl.register);
+router.post('/login', authCtrl.login);
+
+// App API
+router.get('/app', appCtrl.getIcons);
 
 // User API
 router.get('/user', auth, userCtrl.profileRead);
 
 // User Widget API
 router.put('/user', auth, userCtrl.updateWidgets);
-
-// Auth API 
-router.post('/register', authCtrl.register);
-router.post('/login', authCtrl.login);
 
 module.exports = router;
 
