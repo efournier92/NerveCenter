adde3ced23c2d787f6eb3a07f464a805d8e8df4c
Misigno <efournier92@gmail.com>
2017-04-27 20:19:27 -0400

    'add infinite-scroll dependecy

--- app_client/app.min.js
+++ app_client/app.min.js
@@ -2,7 +2,7 @@
 
   angular.module('nerveCenter',
     ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap',
-     'gridster']);
+     'gridster', 'infinite-scroll']);
 
   function config($routeProvider, $locationProvider) {
     $routeProvider
