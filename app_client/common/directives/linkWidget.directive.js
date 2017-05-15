eff9af2debc8a449e9f9d5d6d6480f7503f06557
efournier92 <efournier92@gmail.com>
2017-05-15 19:22:47 -0400

    add linkWidget directive

---
+++ app_client/common/directives/linkWidget.directive.js
@@ -0,0 +1,14 @@
+(function () {
+
+  angular
+    .module('nerveCenter')
+    .directive('clockWidget', clockWidget);
+
+  function clockWidget($window) {
+    return {
+      template: '<ds-widget-clock show-analog theme="dark"></ds-widget-clock>'
+    }
+  }
+
+})();
+
