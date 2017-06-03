d823d1552037d4923d40ecf39471e35d949dc346
efournier92 <efournier92@gmail.com>
2017-06-03 10:11:35 -0400

    start restucturing ngCalc directive

--- public/lib/ng-calc/ng-calc.js
+++ public/lib/ng-calc/ng-calc.js
@@ -1,100 +1,98 @@
-"use strict";
-var ngCalc = angular.module('ng-calc', []) ;
+var ngCalc = angular.module('ng-calc', []);
 
-ngCalc.directive('calculator', function() {
-    var dir = {};
-        dir.restrict   = 'E'; /* restrict this directive to elements */
-        dir.controller = 'CalcuController'; /* controller this directive function belongs */
-        dir.template   = '<div  class="calculator">'
-                          +'<div class="u4 display">'
-                          +'<div class="display-inner">{{out}}</div>'
-                          +'</div>'
-                          +'<button ng-repeat="calkey in mykeys track by $index" ng-click="display(calkey)" '
-                          +'ng-class="{\'u2\': calkey == \'0\' || calkey == \'<-\', \'button-blue\' : calkey == \'=\' , \'button-red\' : calkey == \'c\' }"'
-                          +'class="u1 button button-gray" >'
-                          +'<div ng-if="calkey!=\'<-\'">{{calkey}}</div>'
-                          +'<div ng-if="calkey==\'<-\'">B</div>'
-                          +'</button>'
-                          +'</div>';
-       //dir.templateUrl = 'calculator.html'; //
-    return dir;
+ngCalc.directive('calculator', function () {
+  return {
+    restrict: 'E',
+    controller: 'ngCalcCtrl',
+    template: '<div  class="calculator">'
+              +'<div class="u4 display">'
+              +'<div class="display-inner">{{out}}</div>'
+              +'</div>'
+              +'<button ng-repeat="calkey in mykeys track by $index" ng-click="display(calkey)" '
+              +'ng-class="{\'u2\': calkey == \'0\' || calkey == \'<-\', \'button-blue\' : calkey == \'=\' , \'button-red\' : calkey == \'c\' }"'
+              +'class="u1 button button-gray" >'
+              +'<div ng-if="calkey!=\'<-\'">{{calkey}}</div>'
+              +'<div ng-if="calkey==\'<-\'">B</div>'
+              +'</button>'
+              +'</div>'
+  }
 });
 
 //Register Calculater controller 
-ngCalc.controller('CalcuController',function docal($scope,MathNumbers){
-  
-     $scope.out     = '';
-     $scope.result  = 0;
-    //display function. click 
-     $scope.display = function (number){
-  
-    	if($scope.out!='undefined' && number!='=' && number!='c' && number!='<-'){
-    		$scope.out = $scope.out+number;
-    	}
+ngCalc.controller('ngCalcCtrl',function docal($scope,MathNumbers){
 
-     if($scope.calinput!=''){
+  $scope.out     = '';
+  $scope.result  = 0;
+  //display function. click 
+  $scope.display = function (number){
+
+    if($scope.out!='undefined' && number!='=' && number!='c' && number!='<-'){
+      $scope.out = $scope.out+number;
+    }
+
+    if($scope.calinput!=''){
       switch(number){
 
-         case 'c':
-            //Cancel /reset the display
-            $scope.out = '';
-            break;
+        case 'c':
+          //Cancel /reset the display
+          $scope.out = '';
+          break;
 
-         case '<-':
-           //Backspace operation 
-           $scope.out =  $scope.out.slice(0, -1);
-           break;
+        case '<-':
+          //Backspace operation 
+          $scope.out =  $scope.out.slice(0, -1);
+          break;
 
-         case '=':
-            //do calculation 
-            if($scope.checksymbol($scope.out)){
+        case '=':
+          //do calculation 
+          if($scope.checksymbol($scope.out)){
             $scope.out = eval($scope.out).toString();
-              
-           }
-         break;
 
-         default:
+          }
+          break;
+
+        default:
           break
-         }
-        }
-
+      }
     }
+
+  }
 
   /* 
     Check whether the string contains a restricted charater
     in first or last postion .
     @param strin number
-  */
-    $scope.checksymbol = function (number){
-        
-        var notallow = ['+','-','/','*','.',''] ;
-         if(notallow.indexOf(number.slice(-1))> -1 || notallow.indexOf(number.slice(0,1))>-1){
-            return false;
-         }
-            return true;
-       
-    }
+    */
+  $scope.checksymbol = function (number){
 
-    //Set the keyboard values using the factory method.  
-    $scope.mykeys = MathNumbers.calcnumbers();
-   
+    var notallow = ['+','-','/','*','.',''] ;
+    if(notallow.indexOf(number.slice(-1))> -1 || notallow.indexOf(number.slice(0,1))>-1){
+      return false;
+    }
+    return true;
+
+  }
+
+  //Set the keyboard values using the factory method.  
+  $scope.mykeys = MathNumbers.calcnumbers();
+
 });
 
 
 /*
  Register factory method to set keypads to the calculator.
-*/
+ */
 
 ngCalc.factory('MathNumbers', function() {
-   var factory = {};
-   
-   factory.calcnumbers = function() {
-      var numbs =  [ '7','8','9','0','c','<-',
-                     '4','5','6','.','-','+',
-                     '1','2','3','=','/','*' ];
-        
-      return numbs;
-   }
-   return factory;
+  var factory = {};
+
+  factory.calcnumbers = function() {
+    var numbs =  [ '7','8','9','0','c','<-',
+      '4','5','6','.','-','+',
+      '1','2','3','=','/','*' ];
+
+    return numbs;
+  }
+  return factory;
 }); 
 
