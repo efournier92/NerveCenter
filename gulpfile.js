bc2090a89b5197f448d0b7917ad40d7e6d6202f6
efournier92 <efournier92@gmail.com>
2017-06-08 18:53:12 -0400

    add sass function to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,9 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
 
 gulp.task('bundle', function () {
-  gulp.src(['./app_client/**/*.js', '!./app_client/app.min.js'])
+  gulp.src(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('./app.min.js'))
     .pipe(gulp.dest('app_client'))
@@ -25,49 +16,14 @@ gulp.task('bundle', function () {
 // gulp.task('sass', function(){
 //   return gulp.src('app/scss/styles.scss')
 //     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-//     .pipe(browserSync.reload({
-//       stream: true;
-//     }))
 //     .pipe(gulp.dest('app/css'))
 // });
 
-gulp.task('watch', ['browserSync', 'sass'], function () {
-  gulp.watch(['./app_client/**/*.js', '!./app_client/app.min.js']);
-  gulp.watch('./app_client/*.html');
-  gulp.watch('./app_client/**/*.scss');
+gulp.task('watch', function () {
+  watch(['./app_client/**/*.js', '!./app_client/app.min.js'], function () {
+    gulp.start('scripts');
+  });
 });
-
-gulp.task('useref', function(){
-  return gulp.src('./app_client/*.html')
-    .pipe(useref())
-    // Minifies only if it's a JavaScript file
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('images', function(){
-  return gulp.src('./public/img/ico/*.png')
-  // Caching images that ran through imagemin
-  .pipe(cache(imagemin()))
-  .pipe(gulp.dest('dist/images'))
-});
-
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-})
-
-// gulp.task('clean', function() {
-//   return del.sync('dist');
-// })
-
-// gulp.task('task-name', function(callback) {
-//   runSequence('task-one', 'task-two', 'task-three', callback);
-// });
 
 gulp.task('default', ['bundle', 'watch']);
 
