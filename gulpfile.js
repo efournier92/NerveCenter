6b99b366f7b6b5ca1ba5a89d527602256c79ffd5
efournier92 <efournier92@gmail.com>
2017-06-08 18:51:50 -0400

    add image minification to gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('bundle', function () {
   gulp.src(['./app_client/**/*.js', '!./app_client/app.min.js'])
@@ -16,14 +22,40 @@ gulp.task('bundle', function () {
 // gulp.task('sass', function(){
 //   return gulp.src('app/scss/styles.scss')
 //     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+//     .pipe(browserSync.reload({
+//       stream: true;
+//     }))
 //     .pipe(gulp.dest('app/css'))
 // });
 
-gulp.task('watch', function () {
-  watch(['./app_client/**/*.js', '!./app_client/app.min.js'], function () {
-    gulp.start('scripts');
-  });
+gulp.task('watch', ['browserSync', 'sass'], function () {
+  gulp.watch(['./app_client/**/*.js', '!./app_client/app.min.js']);
+  gulp.watch('./app_client/*.html');
+  gulp.watch('./app_client/**/*.scss');
 });
+
+gulp.task('useref', function(){
+  return gulp.src('./app_client/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
 
 gulp.task('default', ['bundle', 'watch']);
 
