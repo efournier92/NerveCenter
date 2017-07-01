381c6891e26f53edb3916cabe8e3034af80f72d5
efournier92 <efournier92@gmail.com>
2017-07-01 10:08:19 -0400

    add babel options object in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,35 @@ var babel = require('gulp-babel');
 var del = require('del');
 var runSequence = require('run-sequence');
 var watch = require('gulp-watch');
-
 // uglify dependencies
 var uglify = require('gulp-uglify');
 var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
-
 // images dependencies
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 
 gulp.task('concat', function () {
-  gulp.src(['./app_client/**/*.js', '!./app_client/app.min.js'])
+  gulp.src('app_client/**/*.js')
     .pipe(sourcemaps.init())
-    .pipe(concat('./app.min.js'))
-    .pipe(gulp.dest('app_client'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('app_client'));
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('public/dist'));
 });
 
 gulp.task('babel', function () {
-  gulp.src('./app_client/app.min.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'))
+  gulp.src('public/dist/app.min.js')
+  // .pipe(sourcemaps.init())
+  // .pipe(babel(''))
+    .pipe(babel({
+      presets: ['es2015'],
+      minified: true,
+      comments: false,
+      babelrc: false 
+    }))
+  // .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('public/dist'))
 });
 
 gulp.task('uglify', function () {
@@ -54,6 +59,13 @@ gulp.task('images', function () {
     .pipe(gulp.dest('dist/images'))
 });
 
+gulp.task('watch', function () {
+  watch(['./app_client/**/*.(js|html)',
+    '!./app_client/app.min.js'], function () {
+      gulp.start('dev');
+    });
+});
+
 gulp.task('watch', ['browserSync', 'sass'], function () {
   gulp.watch(['./app_client/**/*.js', '!./app_client/app.min.js']);
   gulp.watch('./app_client/*.html');
@@ -64,11 +76,13 @@ gulp.task('clean', function () {
   return del.sync('dist');
 })
 
-gulp.task('default', function (callback) {
+gulp.task('default', ['dev'])
+
+gulp.task('dev', function (callback) {
   runSequence('concat', 'babel', callback);
 });
 
-gulp.task('production', function (callback) {
-  runSequence('concat', 'babel', ['uglify', 'sass', 'images'], ['browserSync', 'watch'], callback);
+gulp.task('prod', function (callback) {
+  runSequence('concat', 'babel', ['uglify', 'sass', 'images'], ['watch'], callback);
 });
 
