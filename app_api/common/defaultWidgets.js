ed6bdd196fead0d45dff68749fa13b0b600e448f
efournier92 <efournier92@gmail.com>
2017-07-09 12:47:48 -0400

    add full formatted defaultWidgets object

--- app_api/common/defaultWidgets.js
+++ app_api/common/defaultWidgets.js
@@ -1,143 +1,288 @@
 var defaultArray = [
-	{
-		"type": "link-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 0,
-		"url": "https://www.google.com/",
-		"icon": "img/ico/A001_Google.png"
-	},
-
-	{
-		"type": "link-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 1,
-		"url": "https://www.youtube.com/",
-		"icon": "img/ico/A002_YouTube.png"
-	},
-
-	{
-		"type": "link-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 2,
-		"url": "https://www.facebook.com/",
-		"icon": "img/ico/A003_Facebook.png"
-	},
-
-	{
-		"type": "link-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 3,
-		"url": "https://www.wikipedia.org/",
-		"icon": "img/ico/A004_Wikipedia.png"
-	},
-
-	{
-		"type": "link-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 4,
-		"url": "https://www.reddit.com/",
-		"icon": "img/ico/A005_Reddit.png"
-	},
-
-	{
-		"type": "link-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 5,
-		"url": "https://www.amazon.com/",
-		"icon": "img/ico/A006_Amazon.png"
-	},
-
-	{
-		"type": "clock-widget",
-		"sizeX": 1,
-		"sizeY": 1,
-		"row": 0,
-		"col": 6
-	},
-
+  {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 0,
+    "url": "https://www.google.com/",
+    "icon": "img/ico/A001_Google.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 1,
+    "url": "https://www.youtube.com/",
+    "icon": "img/ico/A002_YouTube.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 2,
+    "url": "https://www.facebook.com/",
+    "icon": "img/ico/A003_Facebook.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 3,
+    "url": "https://www.wikipedia.org/",
+    "icon": "img/ico/A004_Wikipedia.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 4,
+    "url": "https://www.reddit.com/",
+    "icon": "img/ico/A005_Reddit.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 5,
+    "url": "https://www.amazon.com/",
+    "icon": "img/ico/A006_Amazon.png"
+  }, {
+    "type": "clock-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 0,
+    "col": 6
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 0,
+    "url": "https://twitter.com/",
+    "icon": "img/ico/A007_Twitter.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 1,
+    "url": "https://www.instagram.com/",
+    "icon": "img/ico/A008_Instagram.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 2,
+    "url": "https://www.linkedin.com/",
+    "icon": "img/ico/A009_LinkedIn.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 3,
+    "url": "https://getpocket.com/",
+    "icon": "img/ico/A010_Pocket.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 4,
+    "url": "https://www.netflix.com/",
+    "icon": "img/ico/A011_Netflix.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 5,
+    "url": "https://www.ebay.com/",
+    "icon": "img/ico/A012_Ebay.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 1,
+    "col": 6,
+    "url": "https://www.twitch.tv/",
+    "icon": "img/ico/A013_Twitch.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 0,
+    "url": "https://www.apple.com/",
+    "icon": "img/ico/A014_Apple.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 1,
+    "url": "https://www.imdb.com/",
+    "icon": "img/ico/A015_Imdb.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 2,
+    "url": "https://www.github.com/",
+    "icon": "img/ico/A016_GitHub.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 3,
+    "url": "https://www.pinterest.com/",
+    "icon": "img/ico/A017_Pinterest.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 4,
+    "url": "https://www.paypal.com/",
+    "icon": "img/ico/A018_PayPal.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 5,
+    "url": "https://www.whatsapp.com/",
+    "icon": "img/ico/A019_WhatsApp.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 2,
+    "col": 6,
+    "url": "https://www.craigslist.com/",
+    "icon": "img/ico/A020_CraigsList.png"
+  }, {
+    "type": "nc-calc-widget",
+    "sizeX": 2,
+    "sizeY": 1,
+    "row": 3,
+    "col": 0
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 3,
+    "col": 2,
+    "url": "https://www.nytimes.com/",
+    "icon": "img/ico/A021_NyTimes.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 3,
+    "col": 3,
+    "url": "https://www.walmart.com/",
+    "icon": "img/ico/A022_Walmart.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 3,
+    "col": 4,
+    "url": "http://www.news.google.com/",
+    "icon": "img/ico/A023_News.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 3,
+    "col": 5,
+    "url": "https://trends.google.com/trends/",
+    "icon": "img/ico/A024_Gtrends.png"
+  }, {
+    "type": "link-widget",
+    "sizeX": 1,
+    "sizeY": 1,
+    "row": 3,
+    "col": 6,
+    "url": "https://www.cnn.com/",
+    "icon": "img/ico/A026_Cnn.png"
+  }
 ];
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 0,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 0,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 1,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 1,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 2,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 2,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "nc-calc-widget", "sizeX": 2, "sizeY": 1, "row": 0, "col": 3},
+// {"type": "nc-calc-widget", "sizeX": 2, "sizeY": 1, "row": 0, "col": 3},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 5,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 5,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 6,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 6,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 0,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 0,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 1,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 1,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 2,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 2,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 3,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 3,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 4,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 4,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 5,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 5,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"},
 
-  // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 6,
-  //  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"}
+// {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 1, "col": 6,
+//  "url": "https://www.google.com/", "icon": "img/ico/A001_Google.png"}
 
 // ];
-  // // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 0, "url": "", "icon": ""},
-  // // {"type" : "link-widget", "url" : "https://www.google.com/", "icon" : "img/ico/A001_Google.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.youtube.com/", "icon" : "img/ico/A002_YouTube.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 1},
-  // // {"type" : "link-widget", "url" : "https://www.facebook.com/", "icon" : "img/ico/A003_Facebook.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 2},
-  // // {"type" : "link-widget", "url" : "https://www.wikipedia.org/", "icon" : "img/ico/A004_Wikipedia.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 3},
-  // // {"type" : "link-widget", "url" : "https://www.reddit.com/", "icon" : "img/ico/A005_Reddit.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 4},
-  // // {"type" : "link-widget", "url" : "https://www.amazon.com/", "icon" : "img/ico/A006_Amazon.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 5},
-  // // {"type" : "clock-widget", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0
+// // {"type": "link-widget", "sizeX": 1, "sizeY": 1, "row": 0, "col": 0, "url": "", "icon": ""},
+// // {"type" : "link-widget", "url" : "https://www.google.com/", "icon" : "img/ico/A001_Google.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.youtube.com/", "icon" : "img/ico/A002_YouTube.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 1},
+// // {"type" : "link-widget", "url" : "https://www.facebook.com/", "icon" : "img/ico/A003_Facebook.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 2},
+// // {"type" : "link-widget", "url" : "https://www.wikipedia.org/", "icon" : "img/ico/A004_Wikipedia.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 3},
+// // {"type" : "link-widget", "url" : "https://www.reddit.com/", "icon" : "img/ico/A005_Reddit.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 4},
+// // {"type" : "link-widget", "url" : "https://www.amazon.com/", "icon" : "img/ico/A006_Amazon.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 5},
+// // {"type" : "clock-widget", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0
 // // },{"type" : "nc-calc-widget", "sizeX" : 2, "sizeY" : 1},
-  // // {"type" : "link-widget", "url" : "https://twitter.com/", "icon" : "img/ico/A007_Twitter.png", "sizeX" : 1, "sizeY" : 1, "row" : 1, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.instagram.com/", "icon" : "img/ico/A008_Instagram.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.linkedin.com/", "icon" : "img/ico/A009_LinkedIn.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://getpocket.com/", "icon" : "img/ico/A010_Pocket.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.netflix.com/", "icon" : "img/ico/A011_Netflix.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.ebay.com/", "icon" : "img/ico/A012_Ebay.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.twitch.tv/", "icon" : "img/ico/A013_Twitch.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.apple.com/", "icon" : "img/ico/A014_Apple.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.imdb.com/", "icon" : "img/ico/A015_Imdb.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.github.com/", "icon" : "img/ico/A016_GitHub.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.pinterest.com/", "icon" : "img/ico/A017_Pinterest.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.paypal.com/", "icon" : "img/ico/A018_PayPal.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.whatsapp.com/", "icon" : "img/ico/A019_WhatsApp.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.craigslist.com/", "icon" : "img/ico/A020_CraigsList.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.nytimes.com/", "icon" : "img/ico/A021_NyTimes.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.walmart.com/", "icon" : "img/ico/A022_Walmart.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "http://www.news.google.com/", "icon" : "img/ico/A023_News.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://trends.google.com/trends/", "icon" : "img/ico/A024_Gtrends.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.bing.com/", "icon" : "img/ico/A025_Bing.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.cnn.com/", "icon" : "img/ico/A026_Cnn.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.finance.yahoo.com/", "icon" : "img/ico/A027_YahooFinance.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
-  // // {"type" : "link-widget", "url" : "https://www.baidu.com/", "icon" : "img/ico/A028_Baidu.png, "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0"}
+// // {"type" : "link-widget", "url" : "https://twitter.com/", "icon" : "img/ico/A007_Twitter.png", "sizeX" : 1, "sizeY" : 1, "row" : 1, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.instagram.com/", "icon" : "img/ico/A008_Instagram.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.linkedin.com/", "icon" : "img/ico/A009_LinkedIn.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://getpocket.com/", "icon" : "img/ico/A010_Pocket.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.netflix.com/", "icon" : "img/ico/A011_Netflix.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.ebay.com/", "icon" : "img/ico/A012_Ebay.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.twitch.tv/", "icon" : "img/ico/A013_Twitch.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.apple.com/", "icon" : "img/ico/A014_Apple.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.imdb.com/", "icon" : "img/ico/A015_Imdb.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.github.com/", "icon" : "img/ico/A016_GitHub.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.pinterest.com/", "icon" : "img/ico/A017_Pinterest.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.paypal.com/", "icon" : "img/ico/A018_PayPal.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.whatsapp.com/", "icon" : "img/ico/A019_WhatsApp.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.craigslist.com/", "icon" : "img/ico/A020_CraigsList.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.nytimes.com/", "icon" : "img/ico/A021_NyTimes.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.walmart.com/", "icon" : "img/ico/A022_Walmart.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "http://www.news.google.com/", "icon" : "img/ico/A023_News.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://trends.google.com/trends/", "icon" : "img/ico/A024_Gtrends.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.bing.com/", "icon" : "img/ico/A025_Bing.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.cnn.com/", "icon" : "img/ico/A026_Cnn.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.finance.yahoo.com/", "icon" : "img/ico/A027_YahooFinance.png", "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0},
+// // {"type" : "link-widget", "url" : "https://www.baidu.com/", "icon" : "img/ico/A028_Baidu.png, "sizeX" : 1, "sizeY" : 1, "row" : 0, "col" : 0"}
 // // ];
 
 module.exports.widgetString = JSON.stringify(defaultArray);
