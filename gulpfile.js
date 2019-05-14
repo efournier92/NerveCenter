06e08733bf1ea848a42263bedcc8ef81275428f8
misigno <efournier92@gmail.com>
2019-05-14 17:58:13 -0400

    add dark logo to public folder

--- gulpfile.js
+++ gulpfile.js
@@ -14,58 +14,42 @@ var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 
-var rollup = require('rollup-stream');
-var source = require('vinyl-source-stream');
-
-var webpack = require('gulp-webpack');
-
-gulp.task('babelll', function () {
-  return gulp.src('./app_client/app.min.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./app_client/dist'));
-});
-
-gulp.task('webpack', function() {
-    return gulp.src('app_client/main.js')
-      .pipe(webpack())
-      .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('rollup', function() {
-  return rollup({
-      entry: './app_client/main.js'
-    })
-
-    // output file
-    .pipe(source('app.js'))
-
-    // output folder
-    .pipe(gulp.dest('./app_client'));
-});
-
 gulp.task('concat', function () {
   gulp.src('./app_client/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
     // .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('./app_client'));
+    .pipe(gulp.dest('public/dist'));
 });
 
-gulp.task('babel', function () {
-  gulp.src('app_client/app.min.js')
-  .pipe(sourcemaps.init())
-  .pipe(babel(''))
+gulp.task('babel2', () =>
+  gulp.src('./app_client/**/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
-      // minified: true,
-      // comments: false,
+    }))
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/dist'))
+);
+
+gulp.task('babel', function () {
+  // gulp.src('app_client/app.min.js')
+  gulp.src('./app_client/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    // .pipe(babel(''))
+    .pipe(babel({
+      presets: ['es2015'],
+      minified: true,
+      comments: false,
       // babelrc: false,
       // inputSourceMap: 'public/dist/app.min.js.map',
-      // sourceMaps: false,
+      sourceMaps: true,
       // sourceMapTarget: 'public/dist/app.bun.js.map'
     }))
-  .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('public/dist'))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('public/dist/app.bun.js'))
 });
 
 gulp.task('uglify', function () {
@@ -73,22 +57,7 @@ gulp.task('uglify', function () {
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('sass', function (){
-  return gulp.src('app/scss/styles.scss')
-    .pipe(sass())
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-    .pipe(gulp.dest('app/css'))
-});
-
-gulp.task('images', function () {
-  return gulp.src('./public/img/ico/*.png')
-    .pipe(cache(imagemin()))
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('public/dist'))
 });
 
 gulp.task('watch', function () {
@@ -105,10 +74,10 @@ gulp.task('clean', function () {
 gulp.task('default', ['dev', 'watch']);
 
 gulp.task('dev', function (callback) {
-  gulpSequence('concat', 'babel', callback);
+  gulpSequence('babel', callback);
 });
 
 gulp.task('prod', function (callback) {
-  gulpSequence('concat', 'babel', ['uglify', 'sass', 'images'], ['watch'], callback);
+  gulpSequence('babel2', 'uglify', callback);
 });
 
